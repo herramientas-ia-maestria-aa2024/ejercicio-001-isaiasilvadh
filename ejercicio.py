276bbedebd359dba7344e2a2202d4fe439e4470f
Isaías Silva <isaisailva64@gmail.com>
2024-04-09 21:21:26 -0500

    cambios

--- ejercicio.py
+++ ejercicio.py
@@ -1,12 +1,12 @@
-# Nombre del archivo de texto a leer
+# Ubicación y nombre del archivo
 nombre_archivo = "D:\\UTPL Maestría\\Herramientas IA\\ejercicio-001-isaiasilvadh\\informacion.txt"
 
 try:
-    # Abrir el archivo en modo lectura
+    # Abrir el archivo
     with open(nombre_archivo, 'r') as archivo:
         # Leer todas las líneas del archivo y mostrarlas
         lineas = archivo.readlines()
         for linea in lineas:
-            print(linea.rstrip())  # Imprimir cada línea sin el salto de línea al final
+            print(linea.rstrip())  # Imprimir cada línea
 except FileNotFoundError:
     print(f"El archivo '{nombre_archivo}' no se encontró.")
